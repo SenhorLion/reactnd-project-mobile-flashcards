aca7ffd59350d002ee6dd5df896403335eb9c310
Lionel Adams <adamslionel@hotmail.co.uk>
2018-06-12 20:44:10 +0100

    refactor: Add react-redux connect to mapStateToProps so decks data stay in sync

--- screens/DeckDetail.js
+++ screens/DeckDetail.js
@@ -1,13 +1,6 @@
-import React from 'react';
-import {
-  StyleSheet,
-  Text,
-  View,
-  TouchableOpacity,
-  Platform,
-} from 'react-native';
-import { Constants, AppLoading } from 'expo';
-import Header from '../components/ui/Header';
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, Platform } from 'react-native';
+import { connect } from 'react-redux';
 import { FontAwesome, Ionicons } from '@expo/vector-icons';
 import {
   black,
@@ -17,50 +10,7 @@ import {
   white,
   antiFlashWhite,
 } from '../utils/colors';
-
-export default class DeckDetail extends React.Component {
-  state = {
-    isReady: false,
-  };
-
-  render() {
-    const { navigation } = this.props;
-    const { deck, entryId } = this.props.navigation.state.params;
-    const numOfCards = deck.questions.length;
-    return (
-      <View style={styles.container}>
-        <View>
-          <Text>EntryId - {entryId}</Text>
-          <Text style={styles.title}>{deck.title}</Text>
-          <Text style={styles.cardCount}>{`${numOfCards} ${
-            numOfCards > 1 ? 'cards' : 'card'
-          }`}</Text>
-          <TouchableOpacity
-            style={[styles.button, { marginTop: 20, width: 150 }]}
-            onPress={() => {
-              console.log('Add Card');
-              navigation.navigate('AddCard', { deck, entryId });
-            }}
-          >
-            <FontAwesome name="plus" size={20} color={antiFlashWhite} />
-            <Text style={{ fontSize: 18, color: antiFlashWhite }}>
-              Add Card
-            </Text>
-          </TouchableOpacity>
-          <TouchableOpacity
-            style={[styles.button, { marginTop: 20, width: 150 }]}
-            onPress={() => console.log('Start Quiz')}
-          >
-            <FontAwesome name="comments" size={20} color={antiFlashWhite} />
-            <Text style={{ fontSize: 18, color: antiFlashWhite }}>
-              Start Quiz
-            </Text>
-          </TouchableOpacity>
-        </View>
-      </View>
-    );
-  }
-}
+import ButtonTouchableOpacity from '../components/ui/ButtonTouchableOpacity';
 
 const styles = StyleSheet.create({
   container: {
@@ -77,14 +27,81 @@ const styles = StyleSheet.create({
     fontSize: 20,
     color: gray,
   },
-  button: {
-    flexDirection: 'row',
-    padding: 4,
-    margin: 4,
-    height: 40,
-    width: 80,
-    alignItems: 'center',
-    justifyContent: 'space-around',
-    backgroundColor: purple,
-  },
 });
+
+class DeckDetail extends Component {
+  state = {
+    isReady: false,
+  };
+
+  render() {
+    const { navigation, deck, deckId } = this.props;
+    const numOfCards = deck.questions.length;
+
+    return (
+      <View style={styles.container}>
+        <View>
+          <Text>deckId - {deckId}</Text>
+          <Text style={styles.title}>{deck.title}</Text>
+          <Text style={styles.cardCount}>{`${numOfCards} ${
+            numOfCards > 1 ? 'cards' : 'card'
+          }`}</Text>
+
+          <ButtonTouchableOpacity
+            marginTop={20}
+            width={150}
+            backgroundColor={gray}
+            onPress={() => {
+              console.log('Edit Card navigate');
+              navigation.navigate('EditCard', { deck, deckId });
+            }}
+          >
+            <FontAwesome name="edit" size={20} color={antiFlashWhite} />
+            <Text style={{ fontSize: 18, color: antiFlashWhite }}>
+              Edit Card
+            </Text>
+          </ButtonTouchableOpacity>
+
+          <ButtonTouchableOpacity
+            marginTop={20}
+            width={150}
+            backgroundColor={lightPurple}
+            onPress={() => {
+              console.log('Add Card navigate');
+              navigation.navigate('AddCard', { deck, deckId });
+            }}
+          >
+            <FontAwesome name="plus" size={20} color={antiFlashWhite} />
+            <Text style={{ fontSize: 18, color: antiFlashWhite }}>
+              Add Card
+            </Text>
+          </ButtonTouchableOpacity>
+
+          <ButtonTouchableOpacity
+            marginTop={20}
+            width={150}
+            backgroundColor={lightPurple}
+            onPress={() => console.log('Start Quiz')}
+          >
+            <FontAwesome name="comments" size={20} color={antiFlashWhite} />
+            <Text style={{ fontSize: 18, color: antiFlashWhite }}>
+              Start Quiz
+            </Text>
+          </ButtonTouchableOpacity>
+        </View>
+      </View>
+    );
+  }
+}
+
+const mapStateToProps = ({ decks }, ownProps) => {
+  const { deckId } = ownProps.navigation.state.params;
+  const deck = decks.items[deckId];
+
+  return {
+    deckId,
+    deck,
+  };
+};
+
+export default connect(mapStateToProps)(DeckDetail);
