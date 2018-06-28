969128a726035eec32772a99b91d28e67539a28b
Lionel Adams <adamslionel@hotmail.co.uk>
2018-06-28 11:22:32 +0100

    data: Add random messages for use on quiz completion

--- app/data/words-of-encouragement.js
+++ app/data/words-of-encouragement.js
@@ -1,1 +1,7 @@
-export default ['Try harder, you can do it!', 'Lorem ipsum Dolor'];
+export default [
+  'Try harder, you can do it!',
+  "You haven't failed until you quit trying.",
+  'Mistakes are proof that you are trying.',
+  'The pleasure of success feels best after experiencing the pain of failure.',
+  'The most difficult challenges reap the greatest rewards.',
+];
--- app/data/words-of-praise.js
+++ app/data/words-of-praise.js
@@ -1,1 +1,8 @@
-export default ['Like a Boss, Boss!', 'Well Done Champ!', 'Audere est Facere'];
+export default [
+  'Like a Boss!',
+  'Well Done Champ!',
+  'The initiative you show in your work is outstanding.',
+  'Keep up the good work. I am rooting for you.',
+  'Keep doing the great work that you are doing.',
+  'Even the mighty oak starts as a small acorn.',
+];
