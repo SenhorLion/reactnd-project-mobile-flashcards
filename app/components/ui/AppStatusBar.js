cce3f1ed3264aa44ba706d4d535e9c8b02cb75ea
Lionel Adams <adamslionel@hotmail.co.uk>
2018-06-29 23:56:01 +0100

    fix: Fix for Android statusbar height difference

--- app/components/ui/AppStatusBar.js
+++ app/components/ui/AppStatusBar.js
@@ -2,10 +2,21 @@ import React from 'react';
 import { View, Platform, StatusBar } from 'react-native';
 import { Constants } from 'expo';
 
-const AppStatusBar = ({ backgroundColor, ...props }) => (
-  <View style={{ backgroundColor, height: Constants.statusBarHeight }}>
-    <StatusBar translucent backgroundColor={backgroundColor} {...props} />
-  </View>
-);
+const AppStatusBar = ({ backgroundColor, ...props }) => {
+  const containerStyles = [{ backgroundColor }];
+
+  // NB: In Android, `Constants.statusBarHeight` causes a gap at the top of between the header and StatusBar
+  // to get around this, we only use `statusBarHeight` for `ios`
+  if (Platform.OS === 'ios') {
+    containerStyles.push({
+      height: Constants.statusBarHeight,
+    });
+  }
+  return (
+    <View style={containerStyles}>
+      <StatusBar translucent backgroundColor={backgroundColor} {...props} />
+    </View>
+  );
+};
 
 export default AppStatusBar;
