528cd8083d414a34a9fc5741eb34d1c67a2e720e
Lionel Adams <adamslionel@hotmail.co.uk>
2018-06-30 23:52:14 +0100

    test: Update Deck test and snapshot

--- app/components/Decks/__tests__/Deck.test.js
+++ app/components/Decks/__tests__/Deck.test.js
@@ -21,26 +21,10 @@ it('renders Deck component without crashing', () => {
   expect(DeckComponent).toMatchSnapshot();
 });
 
-it('renders no deck avaialble message when no deck exists', () => {
-  const decksMock = {};
-  const DeckComponent = TestRenderer.create(<Deck deck={decksMock} />).toJSON();
-
-  expect(DeckComponent).toMatchSnapshot();
-});
-
-it('renders deck', () => {
+it('renders deck when no cards exists', () => {
   const deckMock = {
     title: 'React',
-    questions: [
-      {
-        question: 'What is React?',
-        answer: 'A library for managing user interfaces',
-      },
-      {
-        question: 'Where do you make Ajax requests in React?',
-        answer: 'The componentDidMount lifecycle event',
-      },
-    ],
+    questions: [],
   };
 
   const DeckComponent = TestRenderer.create(<Deck deck={deckMock} />).toJSON();
